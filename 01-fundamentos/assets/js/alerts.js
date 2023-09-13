65e74fe8ca441b380a2ef6215a7e49fe907ea8bc
Virginia Mesa <virginia.mesa@auctane.com>
2023-09-13 15:33:13 +0200

    st2. alerts, confirm & prompt

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,10 +1,10 @@
 
 
 let alerta = alert ("Hola mundo") 
-console.log(alerta)
+console.log( alerta )
 
 let nombre = prompt( 'Cual es tu nombre' , 'Sin Nombre' )
 console.log( nombre )
 
 const selection = confirm( '¿Estas seguro de borrar?' )
-console.log(selection)
+console.log( selection )
