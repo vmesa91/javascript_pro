e46f14817d8435e0c6ea102cb005e4f471e13bf9
Virginia Mesa <virginia.mesa@auctane.com>
2023-09-13 13:06:56 +0200

    st2. debugging

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,36 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Hola");
+  });
+
+console.log('Hola Mundo');
+
+let a = 10;
+let b = 10;
+let c = "Hola ";
+let d = "Virginia";
+let x = a + 3;
+
+
+let z = 10, j = 40, k = 30;
+
+const SALUDO = c + d;
+
+/* console.log( x )
+console.warn ( x )
+console.info ( x )
+console.error ( x )
+
+console.warn ( 'x', x )
+console.info ( 'x', x )
+console.error ( 'x', x )
+
+console.log( { x } )
+console.warn ( { x } )
+console.info ( x )
+console.error ( x ) */
+
+console.table({ a, b, c, d, x })
+
+var miNombre = 'Virginia';
