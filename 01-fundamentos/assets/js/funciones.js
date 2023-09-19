947294260a7b9951123b738776a3aeafa815e6ec
Virginia Mesa <virginia.mesa@auctane.com>
2023-09-19 11:33:36 +0200

    st3. functions, desesctructuration

---
+++ 01-fundamentos/assets/js/funciones.js
@@ -0,0 +1,7 @@
+
+function saludar() {
+    console.log('Hola Mundo')
+}
+
+
+saludar()
--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -24,4 +24,31 @@ console.log( 'No. Trajes: ', personaje['trajes'].length )
 console.log( 'Ult. Trajes: ', personaje['trajes'][ personaje['trajes'].length - 1] )
 
 
+// !? More details
+
+// Borrar un elemento
+delete personaje.trajes[0]
+console.log( personaje )
+
+delete personaje.coords
+console.log( personaje )
+
+// Añadir un elemento
+personaje.casado = true;
+
+// Convertir a array
+const entriesPares = Object.entries( personaje )
+console.log( entriesPares )
+
+// Bloqueo de las propiedades de un objeto
+Object.freeze( personaje ); 
+// ! Si lo ponemos como Const, únicamente no nos permitirá asignarlo directamente a otra cosa, pero si podemos modificar las propiedades
+
+
+// Obtener todas las propiedades
+const propiedades = Object.getOwnPropertyNames( personaje );
+const valores = Object.values( personaje )
+
+console.log({ propiedades, valores })
+
 
